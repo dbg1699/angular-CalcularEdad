df2446f692805088d8bb2ec24d0f732e46674360
dbg1699 <dbenitezgonzalez05@gmail.com>
2019-08-10 13:31:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-i4s4zr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-i4s4zr)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  age;
+  showAge;
+
+
+      ageCalculator(){
+      if(this.age){
+        const convertAge = new Date(this.age);
+        const timeDiff = Math.abs(Date.now()- convertAge.getTime());
+        this.showAge= Math.floor ((timeDiff / (1000*360*24))/365);
+      }
+   }
+}
+
